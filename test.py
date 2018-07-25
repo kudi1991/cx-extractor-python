f59da2ed53086411eaaafad731688eb8bf6a9030
zhangle <iamkudi@gmail.com>
2018-07-25 13:44:44 +0800

    test

--- test.py
+++ test.py
@@ -2,8 +2,7 @@
 
 from CxExtractor import CxExtractor
 cx = CxExtractor(threshold=186)
-# html = cx.getHtml("http://www.bbc.com/news/world-europe-40885324")
-html = cx.getHtml("http://news.163.com/17/0810/09/CRFF02Q100018AOR.html")
+html = cx.getHtml("https://www.dianping.com/home-tuku/k3?utm_source=pc_shouye_smalltuku5")
 content = cx.filter_tags(html)
 s = cx.getText(content)
 print(s)
