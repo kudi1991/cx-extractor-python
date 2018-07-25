75d64bc5cdb9e82e4a72d49dd2ef905ed2e71a25
ZhangLe <iamkudi@gmail.com>
2018-07-25 14:41:52 +0800

    兼容python2

--- test.py
+++ test.py
@@ -2,7 +2,8 @@
 
 from CxExtractor import CxExtractor
 cx = CxExtractor(threshold=186)
-html = cx.getHtml("https://www.dianping.com/home-tuku/k3?utm_source=pc_shouye_smalltuku5")
+# html = cx.getHtml("https://www.dianping.com/home-tuku/k3?utm_source=pc_shouye_smalltuku5")
+html = cx.readHtml("Text/001.html")
 content = cx.filter_tags(html)
 s = cx.getText(content)
 print(s)
